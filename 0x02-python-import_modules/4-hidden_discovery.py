aeb6890a6028833e85c55d8010cf18e097099a74
elpastore <abdoulayesadio1997@gmail.com>
2023-10-05 19:13:18 +0000

    test_task4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,4 @@ if __name__ == "__main__":
     import hidden_4
     name = dir(hidden_4)
     for i in name:
-        if i[0:2] != "__":
-            print("{:s}".format(i))
+        print("{:s}".format(i))
