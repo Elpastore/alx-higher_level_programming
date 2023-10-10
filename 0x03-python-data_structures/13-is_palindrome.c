d0462fd797530928908e0d4e359a9a8fd36481ee
elpastore <abdoulayesadio1997@gmail.com>
2023-10-10 11:17:38 +0000

    task13_corrected

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,53 +1,53 @@
 #include "lists.h"
-#include "lists.h"
 #include <stdio.h>
-/**
- * reverse_listint -  reverses a listint_t linked list.
- * @head: the head of list
- * Return: pointer to the first
- */
-listint_t *reverse_listint(listint_t **head)
-{
-	listint_t *prev = NULL;
-	listint_t  *next;
+#include <stdlib.h>
 
-	if (*head == NULL)
-		return (NULL);
-	while (*head != NULL)
-	{
-		next = (*head)->next; /*Store the next node*/
-		(*head)->next = prev; /*Reverse the pointer*/
-		prev = *head; /*Move prev one step ahead*/
-		*head = next; /*Move head one step ahead*/
-	}
-	*head = prev;
-	return (*head);
-}
 /**
  * is_palindrome - function that checks if a SL is a palindrome
- * @head: pointer to head to the list
- * Return: 0 if not palindrome 1 if it is
+ * @head: pointer to head of the list
+ * Return: 0 if not palindrome, 1 if it is
  */
 int is_palindrome(listint_t **head)
 {
 	listint_t *original = *head;
 	listint_t *current = *head;
-	listint_t *reverse = reverse_listint(head);
 	int len_list = 0;
-	int i;
-	if(*head == NULL)
+	int i = 0;
+	int *list;
+
+	if (*head == NULL)
 		return (1);
 	while (current != NULL)
 	{
 		len_list++;
 		current = current->next;
 	}
-	for (i = 0; i < (len_list / 2); i++)
+
+	list = malloc(sizeof(int) * len_list);
+
+	if (list == NULL)
 	{
-		if (original->n != reverse->n)
-			return (0);
-		original = original->next;
-		reverse = reverse->next;
+		return (0);
 	}
-	return (1);
+
+    /*Copy the list into the array*/
+	while (original != NULL)
+	{
+		list[i] = original->n;
+		original = original->next;
+		i++;
+	}
+
+    /*Compare the elements for palindromicity*/
+	for (i = 0; i < len_list / 2; i++)
+	{
+		if (list[i] != list[len_list - i - 1])
+		{
+			free(list);
+			return (0);    /* Not a palindrome*/
+		}
+	}
+	free(list);
+	return (1); /*It's a palindrome*/
 }
+
