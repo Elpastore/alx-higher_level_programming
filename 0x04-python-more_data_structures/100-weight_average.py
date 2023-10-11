a8b3e9fb07c852c7f31ef83569f46d2610a1820a
elpastore <abdoulayesadio1997@gmail.com>
2023-10-11 22:43:29 +0000

    task13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    a = 0
+    b = 0
+    for i in my_list:
+        a += (i[0] * i[1])
+        b += (i[1])
+    average = a / b
+    return (average)
