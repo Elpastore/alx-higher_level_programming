3aca03cf928ab64e81c103442ad496be76d82800
elpastore <abdoulayesadio1997@gmail.com>
2023-10-24 18:07:44 +0000

    task0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module that define a square
+"""
+
+
+class Square:
+
+    """
+    Empty classe Square
+
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,2 @@
+This project  is about OOP and cover class object
+
