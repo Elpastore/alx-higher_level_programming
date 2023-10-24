9f16ce5c4f96a687fa66e1868e2dcb39be45a35a
elpastore <abdoulayesadio1997@gmail.com>
2023-10-24 18:22:21 +0000

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    """
+    This class represents a square
+
+    Attributes:
+    -__size (int):  the size of the square as private
+    """
+    def __init__(self, size):
+        self.__size = size
