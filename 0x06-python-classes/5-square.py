1c9dd72c843a2adf2dfd9acd36ecff91d845890f
elpastore <abdoulayesadio1997@gmail.com>
2023-10-24 20:18:49 +0000

    task5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Module of square"""
+
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+    - __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialization of the square.
+
+        Args:
+            - size (int): The size of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Retrieve the size's square
+
+        Returns:
+            -int:  the size of square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        set the size's square
+
+        Args:
+            - value (int): value to be set
+        Raises:
+            -TypeError: Check for integer value
+            -ValueError: Check for negatif integer
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        method of class Square that return current the square
+        Returns:
+            the square area
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        method that print in stdout the square with the char #
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
