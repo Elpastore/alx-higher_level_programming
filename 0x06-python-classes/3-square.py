308f81eae2c529c8fd097c67d0716ef6f2c68693
elpastore <abdoulayesadio1997@gmail.com>
2023-10-24 19:20:46 +0000

    task3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module of square"""
+
+
+class Square:
+    """
+    This class represents a square.
+
+    Attributes:
+    - __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialization of the square.
+
+        Args:
+            - size (int): The size of the square.
+        Raises:
+            TypeError: check for type error
+            ValueError: check if value is not negatif
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        method of class Square that return current the square
+        Returns:
+            the square area
+        """
+        return self.__size ** 2
