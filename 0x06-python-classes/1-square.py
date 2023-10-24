5a902f4593a01f312c3e25b5c0fa6e113f431ab4
elpastore <abdoulayesadio1997@gmail.com>
2023-10-24 18:32:02 +0000

    task1_v2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 class Square:
     """
-    This class represents a square
+    This class represents a square.
 
     Attributes:
-    -__size (int):  the size of the square as private
-    Methods:
-    -__init__: Constructor that start by given a value of square
-
+    - __size (int): The size of the square.
     """
+
     def __init__(self, size):
         """
-        Iniatialization of the square
+        Initialization of the square.
 
         Args:
-            -size (int): the size of the square
+            - size (int): The size of the square.
         """
         self.__size = size
+
