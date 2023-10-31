ed8bf73332373198a7f4a8ceb40d60f4c468974c
elpastore <abdoulayesadio1997@gmail.com>
2023-10-31 23:31:04 +0000

    task101_secon method

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Locked module
+"""
+
 
 class LockedClass:
+    """
+    Class that restrict the dynamically creating
+    instance with certain condition
+    """
     __slots__ = ["fist_name"]
