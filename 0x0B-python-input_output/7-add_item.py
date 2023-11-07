8cac7f54853a18a479c5456e1792d54aae1d18aa
elpastore <abdoulayesadio1997@gmail.com>
2023-11-07 21:57:29 +0000

    task7_3

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,21 +4,15 @@ script that add argument then save it
 """
 
 
-def main():
-    import sys
-    save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-    load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-    argument = sys.argv
-    try:
-        my_list = load_from_json_file("add_item.json")
-    except:
-        my_list = []
+argument = sys.argv
 
-    for i in argument[1:]:
-        my_list.append(i)
-    save_to_json_file(my_list, "add_item.json")
+my_list = load_from_json_file("add_item.json")
 
-if __name__ == "__main__":
-    main()
+for i in argument[1:]:
+    my_list.append(i)
+save_to_json_file(my_list, "add_item.json")
