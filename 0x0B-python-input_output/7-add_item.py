eddc80ed19575c4cc455b627910fcda7101df79b
elpastore <abdoulayesadio1997@gmail.com>
2023-11-07 22:00:49 +0000

    task7_4

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,12 +5,16 @@ script that add argument then save it
 
 
 import sys
+import os
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 argument = sys.argv
+my_list = []
 
+if not os.path.exists("./add_item.json"):
+    save_json(my_list, "add_item.json")
 my_list = load_from_json_file("add_item.json")
 
 for i in argument[1:]:
