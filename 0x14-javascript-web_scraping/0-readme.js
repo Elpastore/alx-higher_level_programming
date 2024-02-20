b03729ee8f1cb600dee23b0654817b9104713a63
elpastore <abdoulayesadio1997@gmail.com>
2024-02-20 16:30:45 +0000

    task0_v4

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const Filename = process.argv[2];
 fs.readFile(Filename, 'utf8', (err, data) => {
   if (err) {
-  console.error(err);
+    console.error(err);
   }
   else {
     console.log(data);
