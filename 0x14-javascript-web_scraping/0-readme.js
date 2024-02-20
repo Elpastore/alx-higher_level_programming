92f9dbf4c29e25e2ade52f632f5ac9e42a645495
elpastore <abdoulayesadio1997@gmail.com>
2024-02-20 16:38:23 +0000

    previous

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
-fs.readFile(file, 'utf-8', function (err, data) {
+const Filename = process.argv[2];
+fs.readFile(Filename, 'utf8', (err, data) => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
+  console.error(err);
+    return;
   }
+  console.log(data);
 });
