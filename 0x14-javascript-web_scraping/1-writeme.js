55e145124fa29eb7ea7650ba41ce6055cb144cbf
Elpastore <abdoulayesadio1997@gmail.com>
2024-02-20 09:27:32 -0800

    correction_task1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 const Filename = process.argv[2];
-const contentToAdd = process.argv[3]
-fs.appendFile(Filename, contentToAdd,'utf-8', (err) => {
+const contentToAdd = process.argv[3];
+fs.appendFile(Filename, contentToAdd, 'utf-8', (err) => {
   if (err) {
     console.error(err);
   }
